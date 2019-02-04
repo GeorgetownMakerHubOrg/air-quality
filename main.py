332397dfc79f135582c83be0f954d2bb521be8d9
Pascal Girard <pascal@maximumentropy.net>
2019-02-04 13:34:12 -0500

    New main.py - woot

--- main.py
+++ main.py
@@ -3,5 +3,32 @@
 # Professor Colin McCormick & Father Chris Wagner
 #
 # With support from me, Pascal Girard
-import wake
-wake.main()
+#
+import utime
+from machine import Pin, Signal, Timer
+
+pin0 = Pin(0, Pin.IN, Pin.PULL_UP)   	# set GPIO0 as input with pullup
+timer = Timer(0)
+upgrade = False
+
+print('Running main')
+
+def callback(pin):
+	global upgrade
+	upgrade = True
+	return(upgrade)
+
+def run(timer):
+	if not upgrade:
+		print('Running sensors...')
+		import wake
+		wake.main()	
+	else:
+		print('Upgrading...')
+		import webrepl, wifi
+		wifi.init_sta(False)
+		wifi.init_ap(True)
+		webrepl.start()
+
+pin0.irq(trigger=Pin.IRQ_RISING, handler=callback)
+timer.init(period=5000, mode=Timer.ONE_SHOT, callback=run)
